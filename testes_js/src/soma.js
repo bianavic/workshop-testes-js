62f00e8aa1563cd852967df4852ff7e91deecb97
bianavic <bianavic@gmail.com>
2019-06-15 13:40:39 -0300

    it's finally working

--- testes_js/src/soma.js
+++ testes_js/src/soma.js
@@ -1,9 +1,12 @@
 function soma (a, b) {
-  if (a && b) {
-    return a + b
-  } else if (arguments.length > 2 || typeof arguments !== 'number') {
+ if (arguments.length>2) {
     return new Error('voce deve informar 2 numeros e nao pode ser letras')
   }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return new Error('você deve inserir numeros')
+  }
+
+return a+b
 }
 
 module.exports = soma
--- testes_js/test/soma.teste.js
+++ testes_js/test/soma.teste.js
@@ -1,27 +1,28 @@
-const expect = require('chai').expect
-const soma = require('../src/soma')
+const expect = require('chai').expect;
+const soma = require('../src/soma');
 
 describe('Deve retornar a soma de dois numeros', () => {
   it('A soma deve ser uma funcao', () => {
-    expect(soma).to.be.a('function')
+    expect(soma).to.be.a('function');
   })
 
   it('A soma deve retornar 10 quando passamos os parametros 4 e 6', () => {
-    expect(soma(4, 6)).to.be.equal(10)
+    expect(soma(4,6)).to.be.equal(10);
   })
 
   it('A soma deve retornar 7 quando passamos os parametros 4 e 3', () => {
-    expect(soma(4, 3)).to.be.equal(7)
+    expect(soma(4,3)).to.be.equal(7);
   })
-  it('A soma deve retornar um erro quando nao passarmos parametros', () => {
+
+  it('A soma deve retornar um erro quando nao passamos os parametros', () => {
     expect(soma()).to.be.an('error')
   })
 
-  it('A soma deve retornar um erro quando passamos apenas um parametro', () => {
-    expect(soma(4)).to.be.an('error')
+  it('A soma deve retornar um erro quando passamos mais do que dois parametros', () => {
+    expect(soma(1,2,3)).to.be.an('error')
   })
 
-  it('A soma deve retornar um erro quando passamos letras', () => {
-    expect(soma('string')).to.be.an('error')
+  it('A soma deve retornar um erro quando passamos letras nos parametros', () => {
+    expect(soma('a','a')).to.be.an('error')
   })
 })
