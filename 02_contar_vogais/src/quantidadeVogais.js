bdb710c84b39586d1eadd3afc459436a2a57d01e
bianavic <bianavic@gmail.com>
2019-06-17 16:50:07 -0300

    testing error

--- 02_contar_vogais/src/quantidadeVogais.js
+++ 02_contar_vogais/src/quantidadeVogais.js
@@ -1,5 +1,18 @@
-function quantidadeVogais() {
-  return quantidadeVogais
+/* function quantidadeVogais (string) {
+  const palavra = string.match(/[aeiou]/gi)
+  return palavra === null ? 0 : palavra.length
 }
+ */
+/* function quantidadeVogais (palavra) {
+  let quantidade = 0
 
+  for (let letra = 0; letra < quantidadeVogais.length; letra++) {
+    if (palavra === 'a' || palavra === 'e' || palavra === 'i' || palavra === 'o' || palavra === 'u' ||
+  palavra === 'A' || palavra === 'E' || palavra === 'I' || palavra === 'O' || palavra === 'U') {
+    quantidadeVogais++
+    }
+  }
+  // return quantidade
+}
+ */
 module.exports = quantidadeVogais
--- 02_contar_vogais/test/quantidadeVogais.test.js
+++ 02_contar_vogais/test/quantidadeVogais.test.js
@@ -1,8 +1,18 @@
 const expect = require('chai').expect
 const quantidadeVogais = require('../src/quantidadeVogais')
 
-describe('Retornar quantidade de vogais', () => {
-  it('Quantidade de vogais deve ser uma funcao', () => {
-    expect(quantidadeVogais).to.be.a('function')
+/* describe('quantidadeVogais é uma função', () => {
+    it('Verifica se sao vogais', () =>{
+      expect(quantidadeVogais(a)).to.be.an('/[aeiou]/gi')
+
+    })
+ 
+
+  it('Pode conter strings com uma ou mais palavras', () => {
+    expect(quantidadeVogais).to.be.a('string')
+  })
+  it('Retornar erro se for um numero', () => {
+    expect(quantidadeVogais).Throw(error)
   })
 })
+ */
