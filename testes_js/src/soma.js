5a56a25e0d001cdc4e9dd4a969cc4fb46d4f3f72
bianavic <bianavic@gmail.com>
2019-06-19 10:54:50 -0300

    1 passing

---
+++ testes_js/src/soma.js
@@ -0,0 +1,11 @@
+function soma () {
+/* if (arguments.length > 2) {
+    return new Error('voce deve informar 2 numeros e nao pode ser letras')
+  }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return new Error('você deve inserir numeros')
+  }
+
+  return a + b */
+  }  
+module.exports = soma
---
+++ testes_js/test/soma.test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect
+const soma = require('../src/soma')
+
+describe('Deve retornar a soma de dois numeros', () => {
+  it('A soma deve ser uma funcao', () => {
+    expect(soma).to.be.a('function')
+  })
+/*   it('A soma deve retornar 10 quando passamos os parametros 4 e 6', () => {
+    expect(soma(4, 6)).to.be.equal(10)
+  }) */
+})
