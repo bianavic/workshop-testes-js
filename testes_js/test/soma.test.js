c697fe55ae5751b6d70b4d70b4d74936aa32da99
bianavic <bianavic@gmail.com>
2019-06-19 11:17:47 -0300

    3 passing

--- testes_js/test/soma.test.js
+++ testes_js/test/soma.test.js
@@ -8,4 +8,7 @@ describe('Deve retornar a soma de dois numeros', () => {
 it('A soma deve retornar 10 quando passamos os parametros 4 e 6', () => {
     expect(soma(4, 6)).to.be.equal(10)
   })
+  it('A soma deve retornar 7 quando passamos os parametros 4 e 3', () => {
+    expect(soma(4,3)).to.be.equal(7);
+  })
 })
